56cb649e8e78714a85b93eebf0f28dc0dd68c7ef
Firemain <87fugiii@gmail.com>
2024-08-23 15:23:27 +0200

    overlay

--- components/display/MovieCard.js
+++ components/display/MovieCard.js
@@ -1,11 +1,11 @@
 import Image from 'next/image';
-import { PlayIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
+import { PlayIcon, ChevronDownIcon, BoltIcon } from '@heroicons/react/24/solid';
 
 export default function MovieCard({ movie, height, width }) {
   const { title, poster_path, vote_average } = movie;
 
   return (
-    <div className="relative movie-card w-1/3 sm:w-1/5 lg:w-1/6 px-2 overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
+    <div className="relative movie-card w-1/3 sm:w-1/5 lg:w-1/6 overflow-hidden shadow-lg transition-transform duration-300">
       
       <Image
         src={`https://image.tmdb.org/t/p/w500${poster_path}`}
@@ -17,22 +17,22 @@ export default function MovieCard({ movie, height, width }) {
       />
       
       {/* Info Popup on Hover */}
-      <div className="absolute top-0 left-0 right-0 h-full z-10 p-4 bg-[#23043c] bg-opacity-90 text-white shadow-lg rounded-md opacity-0 hover:opacity-100 transition-opacity duration-300">
+      <div className="absolute top-0 left-0 right-0 h-full z-10 p-4 bg-black bg-opacity-70 text-white shadow-lg rounded-md opacity-0 hover:opacity-100 transition-opacity duration-300">
         <h2 className="text-lg font-bold">{title}</h2>
         <p className="text-sm">Rating: {vote_average}</p>
         
         <div className="flex space-x-4 mt-4">
-          <button className="flex items-center bg-white text-black py-1 px-2 rounded ">
+          <button className="flex items-center bg-white text-purple-950 hover:bg-purple-950 hover:text-white transition duration-300 py-1 px-2 rounded ">
             <PlayIcon className="h-5 w-5 mr-2" />
             Spoil
           </button>
-          <button className="flex items-center bg-gray-700 text-white py-1 px-2 rounded">
-            <ChevronDownIcon className="h-5 w-5" />
+          <button className="flex items-center bg-purple-950 text-white hover:bg-white hover:text-purple-950 py-1 px-2 rounded">
+            <BoltIcon className="h-5 w-5" />
           </button>
         </div>
 
         <p className="text-xs mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis.
+          {movie.overview.length > 100 ? movie.overview.substring(0, 100) + "..." : movie.overview}
         </p>
       </div>
     </div>
--- components/display/MovieGrid.js
+++ components/display/MovieGrid.js
@@ -1,22 +1,32 @@
 import Image from 'next/image';
+import { PlayIcon } from '@heroicons/react/24/solid';
 
 export default function MovieGrid({ movies }) {
-    return (
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-6">
-        {movies
-          .filter((movie) => movie.poster_path) // Filtre les films avec poster_path non null ou undefined
-          .map((movie) => (
-            <div key={movie.id} className="relative w-full h-full">
-              <Image
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-                className="rounded-md object-cover"
-                layout="responsive" // S'ajuste proportionnellement à la largeur du conteneur
-                width={200}
-                height={300}
-              />
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-6">
+      {movies
+        .filter((movie) => movie.poster_path && movie.vote_average >= 3) // Filtre les films avec poster_path non null ou undefined et un rating >= 3
+        .map((movie) => (
+          <div key={movie.id} className="relative w-full h-full group">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title || movie.name} // Utilise title ou name selon ce qui est disponible
+              className="rounded-md object-cover"
+              width={200}
+              height={300}
+            />
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4 rounded-md">
+              <h3 className="text-white text-lg font-bold">{movie.title || movie.name}</h3> {/* Affiche le titre ou le nom */}
+              <p className="text-white text-sm">Rating: {movie.vote_average}</p>
+              <button className="flex items-center bg-white text-purple-950 hover:bg-purple-950 hover:text-white transition duration-300 py-1 px-2 rounded mt-2">
+                <PlayIcon className="h-5 w-5 mr-2" />
+                Spoil
+              </button>
             </div>
-          ))}
-      </div>
-    );
-  }
+          </div>
+        ))}
+    </div>
+  );
+}
+
